dc75601394d80a9ef932d37101045bacbac8fb09
Sonya <lutskaya03@bk.ru>
2023-12-04 12:01:31 +0400

    second commFeat1_edit file

--- My_obfusk/New_feature.cs
+++ My_obfusk/New_feature.cs
@@ -1,1 +1,15 @@
 /*Класс для новой ветки с какой-нибудь приколюхой*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace My_obfusk
+{
+    public class New_feature
+    {
+	public New_feature(){};
+    }
+}
